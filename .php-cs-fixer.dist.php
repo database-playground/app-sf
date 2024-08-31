494f89918c27d0e80d715cba35e2e640cfcaadf4
Yi-Jyun Pan <pan93412@gmail.com>
2024-09-01 01:38:32 +0800

    chore: Add php-cs-fixer configuration

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,20 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude([".devenv", ".direnv", "rpc", "public", "var", "vendor"])
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PER-CS' => true,
+        '@PER-CS:risky' => true,
+        '@PHP82Migration' => true,
+        '@PHP80Migration:risky' => true,
+        '@Symfony' => true,
+        '@Symfony:risky' => true,
+        'no_unused_imports' => true,
+    ])
+    ->setFinder($finder)
+    ->setRiskyAllowed(true)
+    ;
