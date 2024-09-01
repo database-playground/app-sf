6f465b24a6fa1b5facbb06d89f2c378250806471
Yi-Jyun Pan <pan93412@gmail.com>
2024-09-02 02:40:58 +0800

    feat(entity): QuestionDifficulty i18n

--- src/Entity/QuestionDifficulty.php
+++ src/Entity/QuestionDifficulty.php
@@ -4,10 +4,23 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
-enum QuestionDifficulty: string
+use Symfony\Contracts\Translation\TranslatableInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+enum QuestionDifficulty: string implements TranslatableInterface
 {
     case Unspecified = 'UNSPECIFIED';
     case Easy = 'EASY';
     case Medium = 'MEDIUM';
     case Hard = 'HARD';
+
+    public function trans(TranslatorInterface $translator, ?string $locale = null): string
+    {
+        return match ($this) {
+            self::Unspecified => $translator->trans('Unspecified', locale: $locale),
+            self::Easy => $translator->trans('Easy', locale: $locale),
+            self::Medium => $translator->trans('Medium', locale: $locale),
+            self::Hard => $translator->trans('Hard', locale: $locale),
+        };
+    }
 }
