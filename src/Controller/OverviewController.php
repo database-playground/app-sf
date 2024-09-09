53fd6d1ead894ac477bdebbcb7e919f47fd235fc
Yi-Jyun Pan <pan93412@gmail.com>
2024-09-09 11:31:29 +0800

    feat(overview): Complete overview algorithm for questions

--- src/Controller/OverviewController.php
+++ src/Controller/OverviewController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Service\PointCalculationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,8 +12,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class OverviewController extends AbstractController
 {
     #[Route('/overview', name: 'app_overview')]
-    public function index(): Response
-    {
-        return $this->render('overview/index.html.twig');
+    public function index(
+        PointCalculationService $pointCalculationService,
+    ): Response {
+        return $this->render('overview/index.html.twig', [
+            'points' => $pointCalculationService->calculate(),
+        ]);
     }
 }
---
+++ src/Service/PointCalculationService.php
@@ -0,0 +1,102 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Entity\Question;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Query\ResultSetMapping;
+use Symfony\Bundle\SecurityBundle\Security;
+
+class PointCalculationService
+{
+    protected User $user;
+    protected static int $BASE_SCORE = 500;
+
+    public function __construct(
+        protected EntityManagerInterface $entityManager,
+        protected Security $security,
+    ) {
+        $user = $this->security->getUser();
+        if (!$user instanceof User) {
+            throw new \RuntimeException('User not logged in.');
+        }
+
+        $this->user = $user;
+    }
+
+    public function calculate(): int
+    {
+        return self::$BASE_SCORE
+            + $this->calculateSolutionQuestionPoints($this->user)
+            + $this->calculateFirstSolutionPoints($this->user);
+    }
+
+    /**
+     * Calculate the total points of the solution events.
+     *
+     * 每位同學基本經驗值500點，成功解一題獲得經驗值增加。易:10點、 中:20點、難:30點
+     *
+     * @param User $user The user to calculate the points for
+     *
+     * @return int The total points of the user
+     */
+    protected function calculateSolutionQuestionPoints(User $user): int
+    {
+        $qb = $this->entityManager->createQueryBuilder();
+        $qb = $qb
+            ->from(Question::class, 'q')
+            ->leftJoin('q.solutionEvents', 'se')
+            ->where(
+                'se.submitter = :submitter',
+                "se.status = 'PASSED'"
+            )
+            ->groupBy('se.question', 'se.submitter')
+            ->select("MAX((
+                case when q.difficulty = 'EASY' then 10
+                when q.difficulty = 'MEDIUM' then 20
+                when q.difficulty = 'HARD' then 30
+                else 0
+                end
+            )) AS point")
+            ->setParameter('submitter', $user);
+
+        /**
+         * @var array<array{point: int}> $result
+         */
+        $result = $qb->getQuery()->getResult();
+
+        return array_reduce($result, fn (int $carry, array $row) => $carry + $row['point'], 0);
+    }
+
+    /**
+     * Calculate the points if the user is the first solver of a question.
+     *
+     * 第一位解題成功者加10點。
+     */
+    protected function calculateFirstSolutionPoints(User $user): int
+    {
+        $nq = $this->entityManager->createNativeQuery("
+            SELECT DISTINCT ON (question_id) submitter_id
+            FROM solution_event
+            WHERE status = 'PASSED'
+            ORDER BY question_id, id;
+        ",
+            (new ResultSetMapping())
+                ->addScalarResult(
+                    'submitter_id',
+                    'sid',
+                    'integer'
+                )
+        );
+
+        /**
+         * @var array<array{sid: int}> $result
+         */
+        $result = $nq->getResult();
+
+        return array_reduce($result, fn (int $carry, array $row) => $carry + ($row['sid'] === $user->getId() ? 10 : 0), 0);
+    }
+}
