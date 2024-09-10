33477da82d4821202cd880252dc5cbf1ffbab803
Yi-Jyun Pan <pan93412@gmail.com>
2024-09-10 14:28:48 +0800

    refactor: Remove unneeded comments

--- src/Repository/SchemaRepository.php
+++ src/Repository/SchemaRepository.php
@@ -17,29 +17,4 @@ class SchemaRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Schema::class);
     }
-
-    //    /**
-    //     * @return Schema[] Returns an array of Schema objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('s')
-    //            ->andWhere('s.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('s.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Schema
-    //    {
-    //        return $this->createQueryBuilder('s')
-    //            ->andWhere('s.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
