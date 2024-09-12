eeeab81fe71aa6f035836677b0b869564b5609b8
Yi-Jyun Pan <pan93412@gmail.com>
2024-09-13 02:10:03 +0800

    fix(controller): Debounce event listener

--- assets/controllers/challenge_solution_video_modal_controller.ts
+++ assets/controllers/challenge_solution_video_modal_controller.ts
@@ -2,19 +2,27 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller<HTMLElement> {
   initialize() {
+    // Safari may open multiple tabs if the event is triggered multiple times
+    // This debounce is to prevent that from happening
+    let debounce: number | undefined;
+
     window.addEventListener("challenge:solution-video:open", (event) => {
-      if (
-        "detail" in event
-        && event.detail
-        && typeof event.detail === "object"
-        && "solutionVideo" in event.detail
-        && event.detail.solutionVideo
-        && typeof event.detail.solutionVideo === "string"
-      ) {
-        window.open(event.detail.solutionVideo, "_blank");
-      } else {
-        throw new Error("Invalid event detail. Expected { solutionVideo: string }");
-      }
+      clearTimeout(debounce);
+
+      debounce = setTimeout(() => {
+        if (
+          "detail" in event
+          && event.detail
+          && typeof event.detail === "object"
+          && "solutionVideo" in event.detail
+          && event.detail.solutionVideo
+          && typeof event.detail.solutionVideo === "string"
+        ) {
+          window.open(event.detail.solutionVideo, "_blank");
+        } else {
+          throw new Error("Invalid event detail. Expected { solutionVideo: string }");
+        }
+      }, 200);
     });
   }
 }
