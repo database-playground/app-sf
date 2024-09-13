b95c0ffbf98ea2481e05524b9188cb22e46a5735
Yi-Jyun Pan <pan93412@gmail.com>
2024-09-14 03:53:30 +0800

    feat(challenge): Add mockup for comment area

--- src/Controller/ChallengeController.php
+++ src/Controller/ChallengeController.php
@@ -14,6 +14,18 @@ use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class ChallengeController extends AbstractController
 {
+    /**
+     * @return MockComment[]
+     */
+    private static function getMockComments(): array
+    {
+        return [
+            new MockComment(0, 'Alice', 'This is a great question!', new \DateTime('now - 1 hour')),
+            new MockComment(1, 'Bob', 'I agree with Alice.', new \DateTime('now - 30 minutes')),
+            new MockComment(2, 'Charlie', 'I disagree with Bob.', new \DateTime('now')),
+        ];
+    }
+
     #[Route('/challenge/{id}', name: 'app_challenge')]
     public function index(
         #[CurrentUser] User $user,
@@ -25,5 +37,37 @@ class ChallengeController extends AbstractController
             'question' => $question,
             'limit' => $questionRepository->count(),
         ]);
+    }
+
+    #[Route('/challenge/{id}/comment', name: 'app_challenge_comment')]
+    public function comment(#[CurrentUser] User $user): Response
+    {
+        return $this->render('challenge/comment.html.twig', [
+            'user' => $user,
+            'comments' => self::getMockComments(),
+        ]);
+    }
+
+    #[Route('/challenge/{id}/comment/{comment_id}', name: 'app_challenge_comment_new')]
+    public function newComment(#[CurrentUser] User $user, string $comment_id): Response
+    {
+        if (!\array_key_exists($comment_id, self::getMockComments())) {
+            throw $this->createNotFoundException('The comment does not exist.');
+        }
+
+        return $this->render('challenge/new_comment.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}
+
+readonly class MockComment
+{
+    public function __construct(
+        public int $id,
+        public string $author,
+        public string $content,
+        public \DateTime $time)
+    {
     }
 }
