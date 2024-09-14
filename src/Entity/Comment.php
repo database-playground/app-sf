cdc7ce5d279c9dc7ba37b982f344b01ee2ae3847
Yi-Jyun Pan <pan93412@gmail.com>
2024-09-14 14:56:45 +0800

    fix(entity): Lifecycle callback for Comment

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -11,6 +11,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CommentRepository::class)]
+#[ORM\HasLifecycleCallbacks]
 class Comment
 {
     use Trait\WithModelTimeInfo;
