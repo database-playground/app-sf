3439393af9fd8599c275f414f3af179c45905fd8
Yi-Jyun Pan <pan93412@gmail.com>
2024-09-14 14:38:10 +0800

    refactor(challenge): Optimize solution video open path

--- assets/controllers/challenge_solution_video_modal_controller.ts
+++ assets/controllers/challenge_solution_video_modal_controller.ts
@@ -1,28 +1,21 @@
 import { Controller } from "@hotwired/stimulus";
+import * as bootstrap from "bootstrap";
 
 export default class extends Controller<HTMLElement> {
-  initialize() {
-    // Safari may open multiple tabs if the event is triggered multiple times
-    // This debounce is to prevent that from happening
-    let debounce: number | undefined;
+  #modal: bootstrap.Modal | undefined;
+  #videoUrl: string | undefined;
 
-    window.addEventListener("challenge:solution-video:open", (event) => {
-      clearTimeout(debounce);
+  connect(): void {
+    this.#modal = new bootstrap.Modal(this.element);
+    this.#videoUrl = this.element.dataset.videoUrl;
+  }
 
-      debounce = setTimeout(() => {
-        if (
-          "detail" in event
-          && event.detail
-          && typeof event.detail === "object"
-          && "solutionVideo" in event.detail
-          && event.detail.solutionVideo
-          && typeof event.detail.solutionVideo === "string"
-        ) {
-          window.open(event.detail.solutionVideo, "_blank");
-        } else {
-          throw new Error("Invalid event detail. Expected { solutionVideo: string }");
-        }
-      }, 200);
-    });
+  open() {
+    if (!this.#videoUrl || !this.#modal) {
+      throw new Error("Not applicable.");
+    }
+
+    window.open(this.#videoUrl, "_blank");
+    this.#modal.hide();
   }
 }
--- src/Controller/ChallengeController.php
+++ src/Controller/ChallengeController.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\Question;
+use App\Entity\SolutionVideoEvent;
 use App\Entity\User;
 use App\Repository\QuestionRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
@@ -37,6 +40,31 @@ class ChallengeController extends AbstractController
             'question' => $question,
             'limit' => $questionRepository->count(),
         ]);
+    }
+
+    #[Route('/challenge/{id}/solution-video', name: 'app_challenge_solution_video', methods: ['GET'])]
+    public function solution_video(
+        Question $question,
+        EntityManagerInterface $entityManager,
+        #[CurrentUser] User $user,
+        #[MapQueryParameter] string $csrf,
+    ): Response {
+        if (!$this->isCsrfTokenValid('challenge-solution', $csrf)) {
+            throw $this->createAccessDeniedException('Invalid path to open solution.');
+        }
+
+        $solutionVideo = $question->getSolutionVideo();
+        if (!$solutionVideo) {
+            throw $this->createNotFoundException('There is no solution video for this question.');
+        }
+
+        $event = (new SolutionVideoEvent())
+            ->setQuestion($question)
+            ->setOpener($user);
+        $entityManager->persist($event);
+        $entityManager->flush();
+
+        return $this->redirect($solutionVideo, Response::HTTP_FOUND);
     }
 
     #[Route('/challenge/{id}/comment', name: 'app_challenge_comment')]
