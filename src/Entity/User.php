a4770ff73f75bae49299508cdfe004cf45eafaee
Yi-Jyun Pan <pan93412@gmail.com>
2024-09-14 13:26:43 +0800

    refactor(entity): Email should be unique

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -40,7 +40,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\ManyToOne(inversedBy: 'users')]
     private ?Group $group = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique: true)]
     private ?string $email = null;
 
     #[ORM\Column(length: 255)]
