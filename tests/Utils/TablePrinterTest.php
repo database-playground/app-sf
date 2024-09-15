9e5d47d4e09876042777ce8751914f81339f9b9f
Yi-Jyun Pan <pan93412@gmail.com>
2024-09-15 12:09:04 +0800

    feat(utils): TablePrinter

---
+++ tests/Utils/TablePrinterTest.php
@@ -0,0 +1,113 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Utils;
+
+use App\Utils\TablePrinter;
+use PHPUnit\Framework\TestCase;
+
+class TablePrinterTest extends TestCase
+{
+    public function testToStringTable(): void
+    {
+        $table = [
+            ['name' => 'Alice', 'age' => 21],
+            ['name' => 'Bob', 'age' => 22],
+            ['name' => 'Charlie', 'age' => 23],
+        ];
+
+        $expected = <<<EOT
+            name     age
+            Alice    21
+            Bob      22
+            Charlie  23
+
+            EOT;
+
+        $this->assertEquals($expected, TablePrinter::toStringTable($table));
+    }
+
+    public function testToNullTable(): void
+    {
+        $table = [
+            ['name' => 'Alice', 'age' => 21],
+            ['name' => 'Bob', 'age' => null],
+            ['name' => 'Charlie', 'age' => 23],
+        ];
+
+        $expected = <<<EOT
+            name     age
+            Alice    21
+            Bob      NULL
+            Charlie  23
+
+            EOT;
+
+        $this->assertEquals($expected, TablePrinter::toStringTable($table));
+    }
+
+    public function testEmptyTable(): void
+    {
+        $table = [];
+
+        $this->assertEquals('', TablePrinter::toStringTable($table));
+    }
+
+    public function testEmptyRow(): void
+    {
+        $table = [
+            ['name' => 'Alice', 'age' => 21],
+            [],
+            ['name' => 'Charlie', 'age' => 23],
+        ];
+
+        $expected = <<<EOT
+            name     age
+            Alice    21
+            NULL     NULL
+            Charlie  23
+
+            EOT;
+
+        $this->assertEquals($expected, TablePrinter::toStringTable($table));
+    }
+
+    public function testEmptyColumn(): void
+    {
+        $table = [
+            ['name' => 'Alice', 'age' => 21],
+            ['name' => 'Bob'],
+            ['name' => 'Charlie', 'age' => 23],
+        ];
+
+        $expected = <<<EOT
+            name     age
+            Alice    21
+            Bob      NULL
+            Charlie  23
+
+            EOT;
+
+        $this->assertEquals($expected, TablePrinter::toStringTable($table));
+    }
+
+    public function testNotPrimitive(): void
+    {
+        $table = [
+            ['name' => 'Alice', 'age' => 21],
+            ['name' => 'Bob', 'age' => ['foo' => 'bar']],
+            ['name' => 'Charlie', 'age' => 23],
+        ];
+
+        $expected = <<<EOT
+            name     age
+            Alice    21
+            Bob      array ('foo' => 'bar')
+            Charlie  23
+
+            EOT;
+
+        $this->assertEquals($expected, TablePrinter::toStringTable($table));
+    }
+}
