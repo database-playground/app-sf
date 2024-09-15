972583664b23687c2d1ad1e9126a8abe8f54a28d
Yi-Jyun Pan <pan93412@gmail.com>
2024-09-15 19:49:36 +0800

    feat(admin): Allow triggering index in Admin panel

--- src/Controller/Admin/QuestionCrudController.php
+++ src/Controller/Admin/QuestionCrudController.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 namespace App\Controller\Admin;
 
 use App\Entity\Question;
+use App\Repository\QuestionRepository;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
@@ -12,7 +16,12 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\CodeEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Meilisearch\Bundle\SearchService;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\HttpFoundation\Response;
+
+use function Symfony\Component\Translation\t;
 
 class QuestionCrudController extends AbstractCrudController
 {
@@ -33,5 +42,28 @@ class QuestionCrudController extends AbstractCrudController
             CodeEditorField::new('answer', 'Answer')->setLanguage('sql'),
             TextField::new('solution_video', 'Solution video')->setFormType(UrlType::class),
         ];
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        $reindex = Action::new('reindex', 'Reindex', 'fa fa-refresh')
+            ->linkToCrudAction('reindex')
+            ->createAsGlobalAction();
+
+        return $actions->add(Crud::PAGE_INDEX, $reindex);
+    }
+
+    public function reindex(
+        AdminUrlGenerator $adminUrlGenerator,
+        SearchService $searchService,
+        QuestionRepository $questionRepository,
+    ): Response {
+        $questionRepository->reindex($searchService);
+        $this->addFlash('success', t('questions.reindex.success'));
+
+        return $this->redirect($adminUrlGenerator
+            ->setController(self::class)
+            ->setAction(Action::INDEX)
+            ->generateUrl());
     }
 }
