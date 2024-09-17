2941ff6769c0e81b50f9e1134ca7f00ba86b6eb4
Yi-Jyun Pan <pan93412@gmail.com>
2024-09-17 19:16:28 +0800

    fix: Upgrade PHPStan

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -111,7 +111,10 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
 
-        return array_unique($roles);
+        $roleUniqueList = array_unique($roles);
+        \assert(array_is_list($roleUniqueList), 'The roles must be an list.');
+
+        return $roleUniqueList;
     }
 
     /**
