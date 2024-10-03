a8a3fb17e45a1c55f716eb68ae3bd6f9f2ca8d98
Yi-Jyun Pan <pan93412@gmail.com>
2024-10-04 01:29:01 +0800

    feat(app): De-/Initialize tooltips for page navigation

--- assets/app/index.ts
+++ assets/app/index.ts
@@ -1,2 +1,19 @@
-import "bootstrap";
+import * as bootstrap from "bootstrap";
 import "./bootstrap.ts";
+
+/**
+ * Initialize tooltips of Bootstrap
+ */
+document.addEventListener("turbo:load", () => {
+  const tooltipTriggerList = document.querySelectorAll("[data-bs-toggle=\"tooltip\"]");
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+  // Destroy tooltips on navigating to a new page
+  document.addEventListener("turbo:before-visit", () => {
+    for (const tooltip of tooltipList) {
+      tooltip.dispose();
+    }
+  }, {
+    once: true,
+  });
+});
