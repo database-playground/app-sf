14ca08dd27c6de730f313ef4eb81bc911fe700c8
Yi-Jyun Pan <pan93412@gmail.com>
2024-11-11 15:28:39 +0800

    revert: Use pretty path in EasyAdmin
    Incompatible with FrankenPHP currently :(
    
    This reverts commit 7e2f1b44d379c8648f36c99fefc8e2f21ed89ec6.

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -18,15 +18,23 @@ use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class DashboardController extends AbstractDashboardController
 {
+    public function __construct(
+        private readonly AdminUrlGenerator $adminUrlGenerator,
+    ) {
+    }
+
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return $this->redirectToRoute('admin_user_index');
+        return $this->redirect(
+            $this->adminUrlGenerator->setController(UserCrudController::class)->generateUrl()
+        );
     }
 
     public function configureDashboard(): Dashboard
