48cabb1f51c41304ed5b1c06e2ba780e961221da
Yi-Jyun Pan <pan93412@gmail.com>
2024-11-28 02:10:46 +0800

    fix(challenge/answer): Handle SchemaExecuteException and QueryExecuteException

--- src/Twig/Components/Challenge/Tabs/AnswerQueryResult.php
+++ src/Twig/Components/Challenge/Tabs/AnswerQueryResult.php
@@ -6,7 +6,10 @@ namespace App\Twig\Components\Challenge\Tabs;
 
 use App\Entity\ChallengeDto\FallableSqlRunnerResult;
 use App\Entity\Question;
+use App\Exception\SqlRunner\QueryExecuteException;
+use App\Exception\SqlRunner\SchemaExecuteException;
 use App\Service\QuestionSqlRunnerService;
+use Psr\Log\LoggerInterface;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
 use function Symfony\Component\Translation\t;
@@ -16,6 +19,7 @@ final class AnswerQueryResult
 {
     public function __construct(
         private readonly QuestionSqlRunnerService $questionSqlRunnerService,
+        private readonly LoggerInterface $logger,
     ) {
     }
 
@@ -27,15 +31,39 @@ final class AnswerQueryResult
     public function getAnswer(): FallableSqlRunnerResult
     {
         try {
-            $resultDto = $this->questionSqlRunnerService->getAnswerResult($this->question);
+            $answerResultDto = $this->questionSqlRunnerService->getAnswerResult($this->question);
+        } catch (SchemaExecuteException $e) {
+            $this->logger->error('Schema Error', [
+                'exception' => $e,
+            ]);
 
-            return (new FallableSqlRunnerResult())->setResult($resultDto);
-        } catch (\Throwable $e) {
+            $errorMessage = t('challenge.errors.schema-error', [
+                '%error%' => $e->getMessage(),
+            ]);
+
+            return (new FallableSqlRunnerResult())->setErrorMessage($errorMessage);
+        } catch (QueryExecuteException $e) {
+            $this->logger->error('Failed to get the answer result', [
+                'exception' => $e,
+            ]);
+
             $errorMessage = t('challenge.errors.answer-query-failure', [
                 '%error%' => $e->getMessage(),
             ]);
 
             return (new FallableSqlRunnerResult())->setErrorMessage($errorMessage);
+        } catch (\Throwable $e) {
+            $this->logger->error('SQL Runner failed when running answer', [
+                'exception' => $e,
+            ]);
+
+            $errorMessage = t('challenge.errors.unavailable', [
+                '%error%' => $e->getMessage(),
+            ]);
+
+            return (new FallableSqlRunnerResult())->setErrorMessage($errorMessage);
         }
+
+        return (new FallableSqlRunnerResult())->setResult($answerResultDto);
     }
 }
