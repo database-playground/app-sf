e47d230ca6ec7cece047dec5ad46de123d477961
Yi-Jyun Pan <pan93412@gmail.com>
2024-11-27 18:54:16 +0800

    feat(events): Reindex the questions when the question is updated

---
+++ src/EventSubscriber/QuestionReindexSubscriber.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventSubscriber;
+
+use App\Entity\Question;
+use App\Repository\QuestionRepository;
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
+use Doctrine\ORM\Event\PostUpdateEventArgs;
+use Doctrine\ORM\Events;
+use Meilisearch\Bundle\SearchService;
+use Psr\Log\LoggerInterface;
+
+#[AsEntityListener(event: Events::postUpdate, method: 'postUpdate', entity: Question::class)]
+final readonly class QuestionReindexSubscriber
+{
+    public function __construct(
+        private QuestionRepository $questionRepository,
+        private SearchService $searchService,
+        private LoggerInterface $logger,
+    ) {
+    }
+
+    public function postUpdate(Question $question, PostUpdateEventArgs $event): void
+    {
+        $this->logger->info("Reindexing question since question #{$question->getId()} has been updated.");
+        $this->questionRepository->reindex($this->searchService);
+    }
+}
