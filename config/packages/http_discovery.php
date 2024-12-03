96aec55096bfeb8e3be6ad01b029466f7303f3a3
Yi-Jyun Pan <pan93412@gmail.com>
2024-12-03 10:38:49 +0800

    refactor(config): Migrate http_discovery.yaml to PHP

---
+++ config/packages/http_discovery.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use Http\Discovery\Psr17Factory;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ServerRequestFactoryInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UploadedFileFactoryInterface;
+use Psr\Http\Message\UriFactoryInterface;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->services()
+        ->alias(RequestFactoryInterface::class, Psr17Factory::class)
+        ->alias(ResponseFactoryInterface::class, Psr17Factory::class)
+        ->alias(ServerRequestFactoryInterface::class, Psr17Factory::class)
+        ->alias(StreamFactoryInterface::class, Psr17Factory::class)
+        ->alias(UploadedFileFactoryInterface::class, Psr17Factory::class)
+        ->alias(UriFactoryInterface::class, Psr17Factory::class);
+};
