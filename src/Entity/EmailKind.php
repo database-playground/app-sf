b8765f575a3251c6cf0ff4d279f12db99cb78cad
Yi-Jyun Pan <pan93412@gmail.com>
2024-12-05 00:23:44 +0800

    feat(email): Add "test" kind

--- src/Entity/EmailKind.php
+++ src/Entity/EmailKind.php
@@ -11,12 +11,14 @@ enum EmailKind: string implements TranslatableInterface
 {
     case Transactional = 'transactional';
     case Marketing = 'marketing';
+    case Test = 'test';
 
     public function trans(TranslatorInterface $translator, ?string $locale = null): string
     {
         return match ($this) {
             self::Transactional => $translator->trans('email-kind.transactional', locale: $locale),
             self::Marketing => $translator->trans('email-kind.marketing', locale: $locale),
+            self::Test => $translator->trans('email-kind.test', locale: $locale),
         };
     }
 }
