d3fdc564be101caf1d91795a230de4ed1bb0dba8
Yi-Jyun Pan <pan93412@gmail.com>
2024-12-05 12:59:11 +0800

    feat(email): EmailCreatedSubscriber

--- src/Entity/EmailKind.php
+++ src/Entity/EmailKind.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use Symfony\Component\Mime\Header\Headers;
 use Symfony\Contracts\Translation\TranslatableInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
@@ -20,5 +21,28 @@ enum EmailKind: string implements TranslatableInterface
             self::Marketing => $translator->trans('email-kind.marketing', locale: $locale),
             self::Test => $translator->trans('email-kind.test', locale: $locale),
         };
+    }
+
+    /**
+     * @throws \InvalidArgumentException
+     */
+    public static function fromEmailHeader(Headers $headers): self
+    {
+        $kind = $headers->getHeaderBody('X-Email-Kind');
+        if (!\is_string($kind)) {
+            throw new \InvalidArgumentException('The email kind header is missing or is invalid type.');
+        }
+
+        return match ($kind) {
+            'transactional' => self::Transactional,
+            'marketing' => self::Marketing,
+            'test' => self::Test,
+            default => throw new \InvalidArgumentException("Invalid email kind: $kind"),
+        };
+    }
+
+    public function addToEmailHeader(Headers $headers): Headers
+    {
+        return $headers->addTextHeader('X-Email-Kind', $this->value);
     }
 }
---
+++ src/EventSubscriber/EmailCreatedSubscriber.php
@@ -0,0 +1,108 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventSubscriber;
+
+use App\Entity\Email as EmailEntity;
+use App\Entity\EmailDeliveryEvent;
+use App\Entity\EmailKind;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Mailer\Event\MessageEvent;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email as EmailMessage;
+
+final readonly class EmailCreatedSubscriber implements EventSubscriberInterface
+{
+    public function __construct(
+        private LoggerInterface $logger,
+        private UserRepository $userRepository,
+        private EntityManagerInterface $entityManager,
+    ) {
+    }
+
+    public function onMessageEvent(MessageEvent $event): void
+    {
+        $message = $event->getMessage();
+        if (!($message instanceof EmailMessage)) {
+            $this->logger->warning('The message is not an instance of Email.', [
+                'message' => $message,
+            ]);
+
+            return;
+        }
+
+        $subject = $message->getSubject();
+        if (!\is_string($subject)) {
+            $this->logger->warning('The message does not have a valid subject.', [
+                'message' => $message,
+                'subject' => $subject,
+            ]);
+
+            return;
+        }
+
+        $body = $message->getHtmlBody();
+        if (!\is_string($body)) {
+            $this->logger->warning('The message does not have an valid HTML body.', [
+                'message' => $message,
+                'body' => $body,
+            ]);
+
+            return;
+        }
+
+        try {
+            $kind = EmailKind::fromEmailHeader($message->getHeaders());
+        } catch (\InvalidArgumentException $exception) {
+            $this->logger->warning('The message does not have a valid email kind.', [
+                'message' => $message,
+                'exception' => $exception,
+            ]);
+
+            return;
+        }
+
+        $email = (new EmailEntity())
+            ->setSubject($subject)
+            ->setContent($body)
+            ->setKind($kind);
+        $this->entityManager->persist($email);
+
+        /**
+         * @var list<Address> $recipients
+         */
+        $recipients = [
+            ...$message->getTo(),
+            ...$message->getCc(),
+            ...$message->getBcc(),
+        ];
+
+        foreach ($recipients as $recipient) {
+            $emailDeliveryEvent = (new EmailDeliveryEvent())
+                ->setToAddress($recipient->getAddress())
+                ->setEmail($email);
+
+            $user = $this->userRepository->findOneBy([
+                'email' => $recipient->getAddress(),
+            ]);
+            if (null !== $user) {
+                $emailDeliveryEvent->setToUser($user);
+            }
+
+            $this->entityManager->persist($emailDeliveryEvent);
+        }
+
+        $this->entityManager->flush();
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            MessageEvent::class => 'onMessageEvent',
+        ];
+    }
+}
