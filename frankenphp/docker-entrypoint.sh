957aede92cb2fd4cdb17a8315adec803382d1708
Yi-Jyun Pan <pan93412@gmail.com>
2024-12-09 01:19:37 +0800

    feat(docker): Add app-sf-worker for asynchronous message handling
    Referenced from https://github.com/dunglas/symfony-docker/pull/681

--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -7,39 +7,45 @@ if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
     fi
 
     if [ -z "$(ls -A 'node_modules/' 2>/dev/null)" ]; then
-      corepack prepare && pnpm install --prod --prefer-frozen-lockfile
+      if node -v; then
+        corepack prepare && pnpm install --prod --prefer-frozen-lockfile
+      else
+        echo "Node.js is not installed. Skipping npm packages installation."
+      fi
     fi
 
-    if grep -q ^DATABASE_URL= .env; then
-        echo "Waiting for database to be ready..."
-        ATTEMPTS_LEFT_TO_REACH_DATABASE=60
-        until [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ] || DATABASE_ERROR=$(php bin/console dbal:run-sql -q "SELECT 1" 2>&1); do
-            if [ $? -eq 255 ]; then
-                # If the Doctrine command exits with 255, an unrecoverable error occurred
-                ATTEMPTS_LEFT_TO_REACH_DATABASE=0
-                break
-            fi
-            sleep 1
-            ATTEMPTS_LEFT_TO_REACH_DATABASE=$((ATTEMPTS_LEFT_TO_REACH_DATABASE - 1))
-            echo "Still waiting for database to be ready... Or maybe the database is not reachable. $ATTEMPTS_LEFT_TO_REACH_DATABASE attempts left."
-        done
+    if [ "${RUN_MIGRATIONS:-true}" = "true" ]; then
+      if grep -q ^DATABASE_URL= .env; then
+          echo "Waiting for database to be ready..."
+          ATTEMPTS_LEFT_TO_REACH_DATABASE=60
+          until [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ] || DATABASE_ERROR=$(php bin/console dbal:run-sql -q "SELECT 1" 2>&1); do
+              if [ $? -eq 255 ]; then
+                  # If the Doctrine command exits with 255, an unrecoverable error occurred
+                  ATTEMPTS_LEFT_TO_REACH_DATABASE=0
+                  break
+              fi
+              sleep 1
+              ATTEMPTS_LEFT_TO_REACH_DATABASE=$((ATTEMPTS_LEFT_TO_REACH_DATABASE - 1))
+              echo "Still waiting for database to be ready... Or maybe the database is not reachable. $ATTEMPTS_LEFT_TO_REACH_DATABASE attempts left."
+          done
 
-        if [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ]; then
-            echo "The database is not up or not reachable:"
-            echo "$DATABASE_ERROR"
-            exit 1
-        else
-            echo "The database is now ready and reachable"
-        fi
+          if [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ]; then
+              echo "The database is not up or not reachable:"
+              echo "$DATABASE_ERROR"
+              exit 1
+          else
+              echo "The database is now ready and reachable"
+          fi
 
-        if [ "$( find ./migrations -iname '*.php' -print -quit )" ]; then
-            php bin/console doctrine:migrations:migrate --no-interaction --all-or-nothing
-        fi
+          if [ "$( find ./migrations -iname '*.php' -print -quit )" ]; then
+              php bin/console doctrine:migrations:migrate --no-interaction --all-or-nothing
+          fi
+      fi
+
+      echo "Updating Meilisearch indexes..."
+      php bin/console meili:clear || true
+      php bin/console meili:import --update-settings || true
     fi
-
-    echo "Updating Meilisearch indexes..."
-    php bin/console meili:clear || true
-    php bin/console meili:import --update-settings || true
 
     setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
     setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
---
+++ worker.Dockerfile
@@ -0,0 +1,76 @@
+# syntax=docker/dockerfile:1
+
+FROM php:8.3-cli AS base
+
+ADD --chmod=0755 https://github.com/mlocati/docker-php-extension-installer/releases/latest/download/install-php-extensions /usr/local/bin/
+
+WORKDIR /app
+VOLUME /app/var/
+
+RUN set -eux; \
+    apt-get update \
+    && apt-get install -y --no-install-recommends \
+      acl=* \
+      file=* \
+      gettext=* \
+      git=* \
+    && rm -rf /var/lib/apt/lists/* \
+    ;
+
+RUN set -eux; \
+    install-php-extensions \
+        @composer \
+        apcu \
+        curl \
+        intl \
+        opcache \
+        zip \
+        redis \
+        pdo_pgsql \
+        sysvsem \
+    ;
+
+# https://getcomposer.org/doc/03-cli.md#composer-allow-superuser
+ENV COMPOSER_ALLOW_SUPERUSER=1
+
+ENV PHP_INI_SCAN_DIR=":$PHP_INI_DIR/app.conf.d"
+
+COPY --link frankenphp/conf.d/10-app.ini $PHP_INI_DIR/app.conf.d/
+COPY --link --chmod=755 frankenphp/docker-entrypoint.sh /usr/local/bin/docker-entrypoint
+
+ENTRYPOINT ["docker-entrypoint"]
+
+# Worker
+FROM base AS worker
+LABEL org.opencontainers.image.source="https://github.com/database-playground/app-sf"
+
+ENV APP_ENV=prod
+ENV APP_DEBUG=0
+
+RUN mv "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
+
+COPY --link frankenphp/conf.d/20-app.prod.ini $PHP_INI_DIR/app.conf.d/
+
+# prevent the reinstallation of vendors at every changes in the source code
+COPY --link composer.* symfony.* package.json* ./
+RUN set -eux; \
+    composer install --no-cache --prefer-dist --no-dev --no-autoloader --no-scripts --no-progress;
+
+# copy sources
+COPY --link . ./
+RUN rm -Rf frankenphp/ config/packages/debug.php config/packages/web_profiler.php
+
+RUN set -eux; \
+    mkdir -p var/cache var/log; \
+    composer dump-autoload --classmap-authoritative --no-dev; \
+    composer dump-env prod; \
+    composer run-script --no-dev post-install-cmd; \
+    chmod +x bin/console; sync;
+
+# build route cache, sass and asset maps
+RUN set -eux; \
+    chmod +x bin/console; sync; \
+    ./bin/console cache:clear; \
+    ./bin/console cache:warmup;
+
+CMD ["php", "bin/console", "messenger:consume", "async", "-vvv"]
