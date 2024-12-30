24872f2c5c21b71c92f289f2cda50bcc0a6017c7
Yi-Jyun Pan <pan93412@gmail.com>
2024-12-30 18:53:22 +0800

    feat(admin): Make "Layout" a ChoiceField

--- src/Controller/Admin/GroupCrudController.php
+++ src/Controller/Admin/GroupCrudController.php
@@ -6,10 +6,13 @@ namespace App\Controller\Admin;
 
 use App\Entity\Group;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+use function Symfony\Component\Translation\t;
 
 class GroupCrudController extends AbstractCrudController
 {
@@ -20,11 +23,26 @@ class GroupCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
+        $availableLayouts = glob(__DIR__.'/../../../templates/overview/layout/*.html.twig');
+        if (false === $availableLayouts) {
+            $availableLayouts = [];
+        }
+
+        $availableLayouts = array_map(static fn ($path) => pathinfo($path, PATHINFO_BASENAME), $availableLayouts);
+        if (0 === \count($availableLayouts)) {
+            $availableLayouts = ['default.html.twig'];
+        }
+
+        // trim the .html.twig extension
+        $availableLayouts = array_map(static fn ($layout) => substr($layout, 0, -10), $availableLayouts);
+
         return [
             IdField::new('id')->hideOnForm(),
             TextField::new('name', 'Name'),
             TextEditorField::new('description', 'Description'),
-            TextField::new('layout', 'Layout'),
+            ChoiceField::new('layout', 'Layout')
+                ->setChoices(array_combine($availableLayouts, $availableLayouts))
+                ->setHelp(t('admin.group.layout.help')),
             DateTimeField::new('created_at', 'Created at')->hideOnForm(),
             DateTimeField::new('updated_at', 'Updated at')->hideOnForm(),
         ];
